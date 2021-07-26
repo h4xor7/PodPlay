9f1c9adc594342b5baeab81813bbad9c9192f51d
unknown <saurabhpandey436@gmail.com>
2021-07-26 21:42:52 +0530

    Initial commit

---
+++ app/src/main/java/com/pandey/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,12 @@
+package com.pandey.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pandey.podplay.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}
